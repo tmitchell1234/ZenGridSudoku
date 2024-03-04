84f0b4bf85042e06360afcfb6039bc346c8d1346
LoganWitte <loganswitte@gmail.com>
2024-03-03 21:26:37 -0500

    Changed css

--- frontend/src/components/RightPane.js
+++ frontend/src/components/RightPane.js
@@ -5,7 +5,7 @@ import LandingButton from './LandingButton';
 function RightPane()
 {
 	return(
-        <div className="Sidebar" style={{width: '576'}}>
+        <div className="Sidebar" style={{width: '576px'}}>
             <LandingButton text="Solve Puzzle" />
             <LandingButton text="Daily Puzzle" />
             <LandingButton text="Head to Head" />
