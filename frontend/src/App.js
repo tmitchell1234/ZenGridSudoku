2c177f8f24874cd399a46a2364064ea4e93167c6
LoganWitte <loganswitte@gmail.com>
2024-03-03 23:59:20 -0500

    Made landing page homepage
    Also removed routing for cards app, left files there incase we can reuse them.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,6 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import React from 'react';
-import LoginPage from './pages/LoginPage';
-import CardPage from './pages/CardPage';
 import LandingPage from './pages/LandingPage';
 
 
@@ -14,9 +12,7 @@ function App()
 	return (
 		<BrowserRouter>
 		<Routes>
-			<Route path="/" index element={<LoginPage />} />
-			<Route path="/cards" index element={<CardPage />} />
-			<Route path="/LandingPage" index element={<LandingPage />} />
+			<Route path="/" index element={<LandingPage />} />
 		</Routes>
 		</BrowserRouter>
 	);
