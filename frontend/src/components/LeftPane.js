b9f44b9cae95c32b6e062358c07366e97daf9469
LoganWitte <loganswitte@gmail.com>
2024-03-11 15:33:27 -0400

    Made page more responsive to logged in status
    Also added temporary test buttons

--- frontend/src/components/LeftPane.js
+++ frontend/src/components/LeftPane.js
@@ -2,20 +2,84 @@ import React from 'react';
 import '../App.css';
 import LandingButton from './LandingButton';
 
+let user_data;
+let firstName;
+let lastName;
+let id;
+
+if(localStorage && localStorage.getItem("user_data")) {
+    user_data = JSON.parse(localStorage.getItem("user_data"));
+    getData();
+    console.log("Data Initialized");
+}
+
+
+function clearData() {
+    localStorage.clear();
+    window.location.reload();
+}
+
+function setData() {
+    let user = {
+        firstName: "Test Name",
+        lastName: "Test last name",
+        id: "Test userID",
+      };
+    localStorage.setItem("user_data", JSON.stringify(user));
+    window.location.reload();
+}
+function getData() {
+    if(user_data) {
+        firstName = user_data.firstName;
+        lastName = user_data.lastName;
+        id = user_data.id;
+        console.log("F: " + firstName + ",L: " + lastName + ",id: " + id);
+    }
+}
+
+function logOut() {
+    localStorage.clear();
+    window.location.reload();
+}
 
 function LeftPane()
 {
+    if(localStorage && localStorage.getItem("user_data")) {
+        return(
+            <div className="Sidebar" style={{width: '15%', borderRight: '1px solid black'} }>
+                <LandingButton text="🎮 Play" set_path="/" />
+                <LandingButton text="📘 About" set_path="/" />
+                <LandingButton text="⚙️ Settings" set_path="/"/>
+                <button className="Landing-button" onClick={logOut}>🔓 Log Out</button>
+                <div style={{marginTop: '30%'}}></div>
+                <button className="Landing-button" onClick={clearData}>TEST CLR DATA</button>
+                <button className="Landing-button" onClick={setData}>TEST SET DATA</button>
+                <button className="Landing-button" onClick={getData}>TEST GET DATA</button>
+                <div className="Landing-Name-Display">
+                    Welcome back, {firstName}!
+                </div>
+            </div>
+        )
+    }
+    else {
+        return(
+            <div className="Sidebar" style={{width: '15%', borderRight: '1px solid black'} }>
+                <LandingButton text="🎮 Play" set_path="/" />
+                <LandingButton text="📝 Sign Up" set_path = "/signuppage" />
+                <LandingButton text="🔑 Sign In" set_path= "/loginpage"/>
+                <LandingButton text="📘 About" set_path="/" />
+                <LandingButton text="⚙️ Settings" set_path="/"/>
+                <div style={{marginTop: '30%'}}></div>
 
+                {/*TEST CODE REMOVE LATER*/}
+                <button className="Landing-button" onClick={clearData}>TEST CLR DATA</button>
+                <button className="Landing-button" onClick={setData}>TEST SET DATA</button>
+                <button className="Landing-button" onClick={getData}>TEST GET DATA</button>
 
-	return(
-        <div className="Sidebar" style={{width: '15%', borderRight: '1px solid black'} }>
-            <LandingButton text="🎮 Play" set_path="/" />
-            <LandingButton text="📝 Sign Up" set_path = "/signuppage" />
-            <LandingButton text="🔑 Sign In" set_path= "/loginpage"/>
-            <LandingButton text="📘 About" set_path="/" />
-            <LandingButton text="⚙️ Settings" set_path="/"/>
-        </div>
-    )
+            </div>
+        )
+    }
+	
 };
 
 export default LeftPane;
