07926b480e72c336cf71cb09e0036540ccb4eb3c
Jeffrey911106 <80051077+Jeffrey911106@users.noreply.github.com>
2024-03-18 21:14:10 -0400

    add getter and setter for user times for each puzzle

--- server.js
+++ server.js
@@ -28,7 +28,6 @@ app.post("/api/createuser", async (req, res, next) => {
   const { username, email, password } = req.body;
 
   const newUser = { Username: username, Email: email, Password: password };
-
   // try to push newuser to Users collection
   try {
     const db = client.db("Sudoku");
@@ -216,6 +215,234 @@ app.post("/api/getpuzzle_hard", async (req, res, next) => {
       error = e.toString();
       var ret = { message: error };
       res.status(500).json(ret);
+    }
+  }
+});
+
+// gets user time of the specific puzzle from the easy set
+app.post("/api/getusertime_easy", async (req, res, next) => {
+    var error = "";
+    const { username, puzzle_number } = req.body;
+  
+    try {
+      const db = client.db("Sudoku");
+
+      const results = await db
+      .collection("user_times_easy")
+      .find({ Username: username, Puzzle_number: puzzle_number})
+      .toArray()
+
+      // check if results is empty, throw error for invalid user/puzzle number with code 501
+      if (results.length == 0) {
+          code = 501;
+          throw new Error("Invalid username/puzzle_number");
+      }
+
+      // otherwise if we find the valid combo of username and puzzle number, return it's usertime
+      var time_easy = results[0].Time_easy;
+
+      var ret = { time_easy: time_easy };
+      res.status(200).json(ret);
+    } catch (e) {
+    // return the error with code 500
+      error = e.toString();
+      var ret = { message: error };
+      res.status(500).json(ret);
+    }
+});
+
+// sets user time of the specific puzzle from the easy set
+app.post("/api/setusertime_easy", async (req, res, next) => {
+    var error = "";
+    const { username, puzzle_number, time_easy } = req.body;
+
+    if (puzzle_number < 1 || puzzle_number > 50) {
+      var ret = { message: "Error, invalid easy puzzle number." };
+      res.status(500).json(ret);
+    } else {
+      // check if string is empty. if it is, send error in return.
+      try {
+          const db = client.db("Sudoku");
+
+          const results = await db
+          .collection("user_times_easy")
+          .find({ Username: username, Puzzle_number: puzzle_number})
+          .toArray();
+
+          // check if results is empty, create usertime for that specific puzzle
+          if (results.length == 0) {
+              const new_user_time = { Username: username, Puzzle_number: puzzle_number, Time_easy: time_easy };
+              const result = await db.collection("user_times_easy").insertOne(new_user_time);
+              var ret = { username: username, puzzle_number: puzzle_number, time_easy: time_easy };
+              res.status(200).json(ret);
+          } else if (results[0].Time_easy > time_easy) {
+              // otherwise check if we need to update the usertime or not
+              const filter = { Username: username, Puzzle_number: puzzle_number };
+              const update = { $set: { Time_easy: time_easy } };  // Use the $set operator to specify the update
+              const result = await db.collection("user_times_easy").updateOne(filter, update);
+              var ret = { message: "Successfully updated." };
+              res.status(200).json(ret);
+          } else {
+            var ret = { message: "No need to update." };
+            res.status(200).json(ret);
+          }
+      } catch (e) {
+          // return the error with code 500
+          error = e.toString();
+          var ret = { message: error };
+          res.status(500).json(ret);
+      }
+    }
+});
+
+// gets user time of the specific puzzle from the medium set
+app.post("/api/getusertime_medium", async (req, res, next) => {
+    var error = "";
+    const { username, puzzle_number } = req.body;
+  
+    try {
+      const db = client.db("Sudoku");
+
+      const results = await db
+      .collection("user_times_medium")
+      .find({ Username: username, Puzzle_number: puzzle_number})
+      .toArray()
+
+      // check if results is empty, throw error for invalid user/puzzle number with code 501
+      if (results.length == 0) {
+          code = 501;
+          throw new Error("Invalid username/puzzle_number");
+      }
+
+      // otherwise if we find the valid combo of username and puzzle number, return it's usertime
+      var time_medium = results[0].Time_medium;
+
+      var ret = { time_medium: time_medium };
+      res.status(200).json(ret);
+    } catch (e) {
+    // return the error with code 500
+      error = e.toString();
+      var ret = { message: error };
+      res.status(500).json(ret);
+    }
+});
+
+// sets user time of the specific puzzle from the medium set
+app.post("/api/setusertime_medium", async (req, res, next) => {
+    var error = "";
+    const { username, puzzle_number, time_medium } = req.body;
+
+    if (puzzle_number < 1 || puzzle_number > 50) {
+      var ret = { message: "Error, invalid medium puzzle number." };
+      res.status(500).json(ret);
+    } else {
+      // check if string is empty. if it is, send error in return.
+      try {
+          const db = client.db("Sudoku");
+
+          const results = await db
+          .collection("user_times_medium")
+          .find({ Username: username, Puzzle_number: puzzle_number})
+          .toArray();
+
+          // check if results is empty, create usertime for that specific puzzle
+          if (results.length == 0) {
+              const new_user_time = { Username: username, Puzzle_number: puzzle_number, Time_medium: time_medium };
+              const result = await db.collection("user_times_medium").insertOne(new_user_time);
+              var ret = { username: username, puzzle_number: puzzle_number, time_medium: time_medium };
+              res.status(200).json(ret);
+          } else if (results[0].Time_medium > time_medium) {
+              // otherwise check if we need to update the usertime or not
+              const filter = { Username: username, Puzzle_number: puzzle_number };
+              const update = { $set: { Time_medium: time_medium } };  // Use the $set operator to specify the update
+              const result = await db.collection("user_times_medium").updateOne(filter, update);
+              var ret = { message: "Successfully updated." };
+              res.status(200).json(ret);
+          } else {
+            var ret = { message: "No need to update." };
+            res.status(200).json(ret);
+          }
+      } catch (e) {
+          // return the error with code 500
+          error = e.toString();
+          var ret = { message: error };
+          res.status(500).json(ret);
+      }
+    }
+});
+
+// gets user time of the specific puzzle from the hard set
+app.post("/api/getusertime_hard", async (req, res, next) => {
+  var error = "";
+  const { username, puzzle_number } = req.body;
+
+  try {
+    const db = client.db("Sudoku");
+
+    const results = await db
+    .collection("user_times_hard")
+    .find({ Username: username, Puzzle_number: puzzle_number})
+    .toArray()
+
+    // check if results is empty, throw error for invalid user/puzzle number with code 501
+    if (results.length == 0) {
+        code = 501;
+        throw new Error("Invalid username/puzzle_number");
+    }
+
+    // otherwise if we find the valid combo of username and puzzle number, return it's usertime
+    var time_hard = results[0].Time_hard;
+
+    var ret = { time_hard: time_hard };
+    res.status(200).json(ret);
+  } catch (e) {
+  // return the error with code 500
+    error = e.toString();
+    var ret = { message: error };
+    res.status(500).json(ret);
+  }
+});
+
+// sets user time of the specific puzzle from the hard set
+app.post("/api/setusertime_hard", async (req, res, next) => {
+  var error = "";
+  const { username, puzzle_number, time_hard } = req.body;
+
+  if (puzzle_number < 1 || puzzle_number > 50) {
+    var ret = { message: "Error, invalid hard puzzle number." };
+    res.status(500).json(ret);
+  } else {
+    // check if string is empty. if it is, send error in return.
+    try {
+        const db = client.db("Sudoku");
+
+        const results = await db
+        .collection("user_times_hard")
+        .find({ Username: username, Puzzle_number: puzzle_number})
+        .toArray();
+
+        // check if results is empty, create usertime for that specific puzzle
+        if (results.length == 0) {
+            const new_user_time = { Username: username, Puzzle_number: puzzle_number, Time_hard: time_hard };
+            const result = await db.collection("user_times_hard").insertOne(new_user_time);
+            var ret = { username: username, puzzle_number: puzzle_number, time_hard: time_hard };
+            res.status(200).json(ret);
+        } else if (results[0].Time_hard > time_hard) {
+            // otherwise check if we need to update the usertime or not
+            const filter = { Username: username, Puzzle_number: puzzle_number };
+            const update = { $set: { Time_hard: time_hard } };  // Use the $set operator to specify the update
+            const result = await db.collection("user_times_hard").updateOne(filter, update);
+            var ret = { message: "Successfully updated." };
+            res.status(200).json(ret);
+        } else {
+          var ret = { message: "No need to update." };
+          res.status(200).json(ret);
+        }
+    } catch (e) {
+        // return the error with code 500
+        error = e.toString();
+        var ret = { message: error };
+        res.status(500).json(ret);
     }
   }
 });
